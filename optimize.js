cf656559de1696b4d492b7577921ad0bd6620bbf
Gabriel Guilherme <gabrielguilhermemartins2218@gmail.com>
2023-04-12 23:12:50 -0300

    Continuação do projeto

--- optimize.js
+++ optimize.js
@@ -1,18 +1,22 @@
 'use strict';
 
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+const AWS = require('aws-sdk')
+const S3 = new AWS.S3()
+module.exports.handler = async ({ Records : records}, context) => {
+    try {
+      await Promise.all(records.map( async record => {
+         const { key } = record.s3.object;
+
+         const image = await S3.getObject
+      }))
+
+      return {
+        statusCode: 201,
+        body: {}
+      }
+    } catch (err) {
+      return err;
+    }
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+ 
